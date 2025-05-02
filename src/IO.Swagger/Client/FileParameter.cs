2258f2552d48796715712446060e1738fb7ec987
leec21316 <leec21316>
2025-05-02 21:56:57 +1000

    Add projects with generated client and regression test cases.

---
+++ src/IO.Swagger/Client/FileParameter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO.Swagger.Client
+{
+    public class FileParameter
+    {
+        public string Name { get; set; }
+
+        public byte[] Data { get; set; }
+
+        public string FileName { get; set; }
+
+        public string ContentType { get; set; }
+
+        public static FileParameter Create(string name, byte[] data, string fileName = null, string contentType = null)
+        {
+            return new FileParameter
+            {
+                Name = name,
+                Data = data,
+                FileName = fileName ?? "no_file_name_provided",
+                ContentType = contentType ?? "application/octet-stream"
+            };
+        }
+    }
+}
